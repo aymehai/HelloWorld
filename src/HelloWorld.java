2739b0415b9f6de2b8c7fbe715b833382b13f8f6
student <student@GBTC440J11LX.montgomerycollege.edu>
2017-05-24 13:25:25 -0400

    first project

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	
+	/* This is a multi-line comment
+	 * Author: Aymen Mehai
+	 * Date 05/24/2017
+	 * this is our first program
+	 */
+	
+	
+	public static void main(String[] args) {
+		//This is a one line comment
+		System.out.println("Hellow World!");
+	}
+}
